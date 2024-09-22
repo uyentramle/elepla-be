feb10d3c8b3d2394c32023c4e3743443a3b88253
i'm tum <minhtam250102@gmail.com>
2024-09-23 03:49:17 +0700

    add facebook service into project

---
+++ Elepla.Service/Utils/FacebookService.cs
@@ -0,0 +1,42 @@
+﻿using Elepla.Service.Common;
+using Elepla.Service.Interfaces;
+using Elepla.Service.Models.ViewModels.AuthViewModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elepla.Service.Utils
+{
+    public class FacebookService : IFacebookService
+    {
+        public AppConfiguration _facebookSettings;
+
+        public FacebookService(AppConfiguration facebookSettings)
+        {
+            _facebookSettings = facebookSettings;
+        }
+
+        public async Task<FacebookUserInfo> GetUserInfoFromFacebookAsync(string accessToken)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var url = $"https://graph.facebook.com/me?fields=id,email,first_name,last_name,picture&access_token={accessToken}";
+                    var response = await httpClient.GetStringAsync(url);
+                    var userInfo = JsonConvert.DeserializeObject<FacebookUserInfo>(response);
+
+                    return userInfo;
+                }
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+    }
+}
