27dc8c9150ba67d8696f11a0f458f34c62fd9db3
i'm tum <minhtam250102@gmail.com>
2024-09-27 17:03:53 +0700

    add Swagger support with JWT Bearer token authorization

--- Elepla.API/DependencyInjection.cs
+++ Elepla.API/DependencyInjection.cs
@@ -10,6 +10,7 @@ using Elepla.Service.Utils;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 namespace Elepla.API
@@ -40,6 +41,35 @@ namespace Elepla.API
                 };
             });
 
+            // Add Swagger 
+            services.AddSwaggerGen(option =>
+            {
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
+            });
+
+
             services.AddCors(options =>
             {
                 options.AddDefaultPolicy(builder =>
@@ -55,8 +85,8 @@ namespace Elepla.API
             services.AddTransient<SeedData>();
             services.AddControllers();
             services.AddEndpointsApiExplorer(); // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            services.AddSwaggerGen();
             services.AddMemoryCache();
+
 
             return services;
         }
