58cae9668002c62b8ae166a860c199f954cef0f8
i'm tum <minhtam250102@gmail.com>
2024-09-28 23:30:54 +0700

    add view user profile function

---
+++ Elepla.API/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Elepla.Service.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Elepla.API.Controllers
+{
+    public class AccountController : BaseController
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        #region View User Profile
+        [HttpGet]
+        public async Task<IActionResult> GetUserProfileAsync(string userId)
+        {
+            var response = await _accountService.GetUserProfileAsync(userId);
+            if (response.Success)
+            {
+                return Ok(response);
+            }
+            return BadRequest(response);
+        }
+        #endregion
+    }
+}
--- Elepla.Service/Services/AccountService.cs
+++ Elepla.Service/Services/AccountService.cs
@@ -2,7 +2,10 @@
 using Elepla.Repository.Interfaces;
 using Elepla.Service.Common;
 using Elepla.Service.Interfaces;
+using Elepla.Service.Models.ResponseModels;
+using Elepla.Service.Models.ViewModels.AccountViewModels;
 using Elepla.Service.Utils;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Caching.Memory;
 using System;
 using System.Collections.Generic;
@@ -25,5 +28,42 @@ namespace Elepla.Service.Services
             : base(unitOfWork, mapper, timeService, passwordHasher, emailSender, smsSender, cache)
         {
         }
+
+        #region Manage User Profile
+        // Get user profile
+        public async Task<ResponseModel> GetUserProfileAsync(string userId)
+        {
+            try
+            {
+                var user = await _unitOfWork.AccountRepository.GetByIdAsync(id: userId, includeProperties: "Role,Avatar,Background");
+                if (user == null)
+                {
+                    return new ResponseModel { Success = false, Message = "User not found." };
+                }
+
+                var userProfileDTO = _mapper.Map<ViewUserProfileDTO>(user);
+                userProfileDTO.Role = user.Role.Name;
+                userProfileDTO.Avatar = user.Avatar?.ImageUrl;
+                userProfileDTO.Background = user.Background?.ImageUrl;
+
+                return new SuccessResponseModel<object>
+                {
+                    Success = true,
+                    Message = "User profile found.",
+                    Data = userProfileDTO
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ErrorResponseModel<object>
+                {
+                    Success = false,
+                    Message = "An error occurred while retrieving the user profile.",
+                    Errors = new List<string> { ex.Message }
+                };
+            }
+        }
+
+        #endregion
     }
 }
