e19d094f6b37d5d7844579a9f8736642b35787e5
thangnguyen8520 <130530198+thangnguyen8520@users.noreply.github.com>
2024-10-17 16:08:51 +0700

    view and submit feedback

---
+++ Elepla.Service/Models/ViewModels/FeedbackViewModels/ViewFeedbackDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elepla.Service.Models.ViewModels.FeedbackViewModels
+{
+    public class ViewFeedbackDTO
+    {
+        public string FeedbackId { get; set; }
+        public string Content { get; set; }
+        public int? Rate { get; set; }
+        public string TeacherName { get; set; }
+        public string PlanbookTitle { get; set; }
+    }
+}
